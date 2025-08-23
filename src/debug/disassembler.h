f2e0377d72b28332f1a891d4a74c34030194a907
Harsh1s <harsh.raj.cd.ece21@itbhu.ac.in>
2025-08-23 22:12:47 +0530

    Decode immediate load instructions

--- src/debug/disassembler.h
+++ src/debug/disassembler.h
@@ -18,3 +18,4 @@ DecodedInstruction decode(std::uint16_t address, std::uint16_t opcode);
 std::vector<DecodedInstruction> disassemble(std::uint16_t start_address, const std::vector<std::uint8_t>& bytes);
 std::string format_instruction(const DecodedInstruction& instruction);
 bool is_jump_or_call(std::uint16_t opcode);
+bool touches_register(std::uint16_t opcode, std::uint8_t register_index);
